720a87defb82cb64b3e6cbe0778b2e8982020f50
Lucas Viana <lucasvfv@gmail.com>
2023-05-12 15:15:41 -0300

    fix: regex comments added

--- src/app/schemas/transactionInputSchema.ts
+++ src/app/schemas/transactionInputSchema.ts
@@ -3,6 +3,8 @@ import * as yup from 'yup';
 export const transactionSchema = yup.object({
   amount: yup
     .string()
+    //(?!^[0.,]+$) => valor diferente de zero, "." e ",""
+    //[^-\+\s]*$ => classe de carascteres negadas: "-", "+" e "\s"
     .matches(/^(?!^[0.,]+$)[^-\+\s]*$/, {
       message: 'Valor precisa ser acima de zero',
     })
